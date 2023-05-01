9120efb027f4e6c98ce06ccee90f8ca748409439
imkiwifruit <127142973+hong0503@users.noreply.github.com>
2023-05-02 07:24:47 +0800

    Update Rectangle.cpp

--- 11.11-6/Rectangle.cpp
+++ 11.11-6/Rectangle.cpp
@@ -1,37 +1,37 @@
+#include"Rectangle.h"
 #include<iostream>
-#include "Rectangle.h"
 using namespace std;
 int Rectangle::numOfRect = 0;
 Rectangle::Rectangle() {
-    width = 0;
-    height = 0;
+	width = 0;
+	height = 0;
 }
 Rectangle::~Rectangle() {
-    numOfRect--;
+	numOfRect--;
 }
 Rectangle::Rectangle(double w, double h) {
-    width = w;
-    height = h;
-    numOfRect++;
+	width = w;
+	height = h;
+	numOfRect++;
 }
 double Rectangle::getWidth() const {
-    return width;
-}
-void Rectangle::setWidth(double w) {
-    width = w;
+	return width;
 }
 double Rectangle::getHeight() const {
-    return height;
-}
-void Rectangle::setHeight(double h) {
-    height = h;
+	return height;
 }
 double Rectangle::getArea() const {
-    return width * height;
-}
-void Rectangle::print() {
-    cout << height << " " << width << " " << getArea() << endl;
+	return width * height;
 }
 int Rectangle::getNumOfRect() {
-    return numOfRect;
+	return numOfRect;
+} 
+void Rectangle::setWidth(double w) {
+	width = w;
+}
+void Rectangle::setHeight(double h) {
+	height = h;
+}
+void Rectangle::print() {
+	cout << height <<  " " << width << " " << getArea() << endl;
 }
